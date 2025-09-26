346a08010dd2b6a49e78bd2fea12faa92ae6b91c
Yolomasterful <Yolomasterful@outllook.com>
2025-09-26 08:29:30 -0600

    Update

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,15 +9,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.TextField
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+// import androidx.compose.ui.*
+
+/* import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width */
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 
 
 class MainActivity : ComponentActivity() {
@@ -54,7 +66,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun LoginScreen() {
     val email = remember { mutableStateOf("") } 
 
-    Column {
+    Column(modifier=Modifier.fillMaxSize)
+    {
         TextField(
             value = email.value,
             onValueChange = { email.value = it },
