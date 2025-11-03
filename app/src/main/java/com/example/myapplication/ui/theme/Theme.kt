1799a1dde351926f90791a3f9f840fc70ad1d085
heshnaB <heshna599@gmail.com>
2025-11-03 00:53:43 -0700

    Organized the code according to the folder structures, and imported the login screen into another folder.

--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.ui.graphics.Color
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -10,17 +11,41 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Modifier
+import androidx.compose.material3.TextField
+import androidx.compose.material3.Text
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.foundation.border
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.layout.fillMaxWidth
+
+
+
+
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = MacewanRed,
+    secondary = DefaultWhite,
+    tertiary = MacewanRed
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary =LightGreyDefault,
+    secondary = LightGreyDefault,
+    //tertiary = DefaultWhite,
+    onPrimary = BlackDefault,
+    onSecondary = BlackDefault,
+    background = MacewanRed,
+    onBackground = BlackDefault,
+    surface = DefaultWhite,
+    onSurface = BlackDefault
+
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,11 +58,44 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+// Helper function for the app text field
+@Composable
+fun AppwhiteTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholderText: String = "",
+    modifier: Modifier = Modifier,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    trailingIcon: @Composable (() -> Unit)? = null,
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        placeholder =  { Text(placeholderText)},
+        modifier= modifier.fillMaxWidth(),
+        keyboardOptions = keyboardOptions,
+        shape = RoundedCornerShape(0.dp),
+        visualTransformation = visualTransformation,
+        trailingIcon = trailingIcon,
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                focusedTextColor = MaterialTheme.colorScheme.onBackground,
+                cursorColor = MaterialTheme.colorScheme.primary,
+                focusedIndicatorColor = MaterialTheme.colorScheme.onPrimary,
+                unfocusedIndicatorColor = MaterialTheme.colorScheme.onPrimary
+        )
+    )
+}
+
+
+
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
--- app/src/main/java/com/example/myapplication/ui/theme/Type.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Type.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication.ui.theme
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,6 +18,33 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+//    uTypetext = TextStyle(
+//
+//            fontSize = 28.sp,
+//        fontWeight = FontWeight(400),
+//        color = Color.Black
+//)
+)
+
+
+val Typography.uTypetext: TextStyle
+    @Composable
+    get() = TextStyle(
+            fontSize = 28.sp,
+            fontWeight = FontWeight(400),
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+
+val Typography.uTitletext: TextStyle
+    @Composable
+    get() = TextStyle(
+        fontSize = 36.sp,
+        fontWeight = FontWeight(600),
+        color = MaterialTheme.colorScheme.onPrimary
+    )
+
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -31,4 +61,3 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
-)
