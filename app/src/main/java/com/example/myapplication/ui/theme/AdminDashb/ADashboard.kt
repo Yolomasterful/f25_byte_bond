931dddc400d2ba43baabab7f8381f983940d3929
heshnaB <heshna599@gmail.com>
2025-11-03 20:12:55 -0700

    Created a main dashboard layout for all users.

--- app/src/main/java/com/example/myapplication/ui/theme/AdminDashb/ADashboard.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/AdminDashb/ADashboard.kt
@@ -1,56 +1,29 @@
 package com.example.myapplication.ui.theme.AdminDashb
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.ui.res.painterResource
-import com.example.myapplication.R
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
 import com.example.myapplication.ui.theme.uTypetext
-import com.example.myapplication.ui.theme.uTitletext
 import androidx.compose.material3.ButtonDefaults
-
-
+import com.example.myapplication.ui.theme.MainDashboard
 
 @Composable
+fun ADashboard(navController: NavHostController) {
+    MainDashboard(navController = navController) {
 
-fun ADashboard(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-    ) {
-        Spacer(modifier = Modifier.height(177.dp))
-        Text("Welcome",
-            modifier = Modifier
-                .fillMaxWidth(),
-            style = MaterialTheme.typography.uTitletext,
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(60.dp))
         Button(
-            onClick = {},
-            shape = RoundedCornerShape(10.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = MaterialTheme.colorScheme.surface
+                onClick = {},
+                shape = RoundedCornerShape(10.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.surface
             ),
             modifier = Modifier
                 .width(297.dp)
@@ -61,7 +34,8 @@ fun ADashboard(
                 style = MaterialTheme.typography.uTypetext
             )
         }
-        Spacer(modifier = Modifier.height(61.dp))
+
+        Spacer(modifier = Modifier.height(60.dp))
         Button(
             onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -76,7 +50,7 @@ fun ADashboard(
                 text = "Book a room",
                 style = MaterialTheme.typography.uTypetext)
         }
-        Spacer(modifier = Modifier.height(61.dp))
+        Spacer(modifier = Modifier.height(60.dp))
         Button(
             onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -91,6 +65,7 @@ fun ADashboard(
                 text = "Assign proctors",
                 style = MaterialTheme.typography.uTypetext)
         }
-
+        Spacer(modifier = Modifier.height(60.dp))
+        }
     }
-}
+
--- app/src/main/java/com/example/myapplication/ui/theme/ProctorDashb/PDashboard.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/ProctorDashb/PDashboard.kt
@@ -1,51 +1,23 @@
 package com.example.myapplication.ui.theme.ProctorDashb
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.ui.res.painterResource
-import com.example.myapplication.R
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.text.style.TextAlign
 import androidx.navigation.NavHostController
 import com.example.myapplication.ui.theme.uTypetext
-import com.example.myapplication.ui.theme.uTitletext
 import androidx.compose.material3.ButtonDefaults
-
-
+import com.example.myapplication.ui.theme.MainDashboard
 
 @Composable
-
-fun PDashboard(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier
-    ) {
-        Spacer(modifier = Modifier.height(177.dp))
-        Text("Welcome",
-            modifier = Modifier
-                .fillMaxWidth(),
-            style = MaterialTheme.typography.uTitletext,
-            textAlign = TextAlign.Center
-        )
-        Spacer(modifier = Modifier.height(60.dp))
+fun PDashboard(navController: NavHostController) {
+    MainDashboard(navController = navController) {
         Button(
             onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -61,7 +33,7 @@ fun PDashboard(
                 style = MaterialTheme.typography.uTypetext
             )
         }
-        Spacer(modifier = Modifier.height(61.dp))
+        Spacer(modifier = Modifier.height(60.dp))
         Button(
             onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -77,7 +49,7 @@ fun PDashboard(
                 style = MaterialTheme.typography.uTypetext)
         }
 
-        Spacer(modifier = Modifier.height(61.dp))
+        Spacer(modifier = Modifier.height(60.dp))
         Button(
             onClick = {},
             shape = RoundedCornerShape(10.dp),
@@ -92,6 +64,7 @@ fun PDashboard(
                 text = "My Schedule",
                 style = MaterialTheme.typography.uTypetext)
         }
+        Spacer(modifier = Modifier.height(60.dp))
 
     }
 }
