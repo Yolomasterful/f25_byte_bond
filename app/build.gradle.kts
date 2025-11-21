3583482a648e58fe332d0e7dd0f63b5978b1b9a0
SemheAigbefo <aigbefos@mymacewan.ca>
2025-11-20 20:24:31 -0700

    Add Firebase dependencies + config updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.gms.google-services")
+
 }
 
 android {
@@ -50,6 +52,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.foundation.layout)
     implementation(libs.androidx.material3)
+    implementation(platform("com.google.firebase:firebase-bom:34.6.0")) //import BoM for firebase
+    implementation("com.google.firebase:firebase-auth") //dependency for firebase auth library
     //implementation(libs.androidx.compose.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
@@ -61,6 +65,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.8.3")
     //implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material3:material3:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.4")
+
 
 
 }
